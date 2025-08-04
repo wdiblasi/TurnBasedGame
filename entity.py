68231a2f3aed3a7d4c9a8f348e779674dfcac422
wdiblasi <bdiblasi8@gmail.com>
2025-08-04 15:44:37 -0400

    Added entity.py file with basic contents

---
+++ entity.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+import asyncio
+
+class Entity(ABC):
+    def __init__(self, max_health, hp, timer_cap, position):
+        self.max_health = max_health
+        self.hp = self.max_health
+        self.timer_cap = timer_cap
+        self.timer = 0
+        self.position = position
+
+    def inc_timer(self):
+        self.timer += 1
+        if self.timer >= self.timer_cap:
+            self.take_turn()
+
+    @abstractmethod
+    async def take_turn(self):
+        pass
+
+    @abstractmethod
+    async def base_attack(self):
+        pass
+
+    @abstractmethod
+    async def special_move(self):
+        pass
